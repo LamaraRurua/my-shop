97cde0042d9e93758b5e4c223fba0d9c96fa5d61
rurua.lamara@gtu.ge <rurua.lamara@gtu.ge>
2025-01-17 21:46:21 +0400

    add file

--- src/app/cart-item.model.ts
+++ src/app/cart-item.model.ts
@@ -1,6 +1,22 @@
 import { Product } from "./product.model";
+import { Dress } from "./product.model";
+import { Trousers } from "./product.model";
+import {Hat } from "./product.model";
+
 
 export interface CartItem {
     product: Product;
+    quantity: number;
+}
+export interface CartItem {
+    product: Dress;
+    quantity: number;
+}
+export interface CartItem {
+    product: Trousers;
+    quantity: number;
+}
+export interface CartItem {
+    product: Hat;
     quantity: number;
 }
--- src/app/product.model.ts
+++ src/app/product.model.ts
@@ -1,5 +1,24 @@
 export interface Product {
     id: number;
     name: string;
+    size: string;
+    price: number;
+}
+export interface Dress {
+    id: number;
+    name: string;
+    size: string;
+    price: number;
+}
+export interface Trousers {
+    id: number;
+    name: string;
+    size: string;
+    price: number;
+}
+export interface Hat {
+    id: number;
+    name: string;
+    size: string;
     price: number;
 }
